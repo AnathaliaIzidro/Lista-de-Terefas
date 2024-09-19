12c338c8ee8788a802bffaea85f29e730cfdd780
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-09-19 15:43:34 -0300

    Update app.js
    Versão 1.1.2

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const app = Vue.createApp({
     data() {
         return {
-            newTask: '',
+            newTask: {
+                text: '',
+                date: '',
+                time: ''
+            },
             tasks: []
         }
     },
     methods: {
         addTask() {
-            if (this.newTask.trim()) {
+            if (this.newTask.text.trim()) {
                 this.tasks.push({
-                    text: this.newTask,
+                    text: this.newTask.text,
+                    date: this.newTask.date,
+                    time: this.newTask.time,
                     completed: false,
                     editing: false,
-                    editText: ''
+                    editText: '',
+                    editDate: '',
+                    editTime: ''
                 });
-                this.newTask = '';
+                this.newTask = { text: '', date: '', time: '' };
             }
         },
         removeTask(index) {
@@ -23,12 +31,24 @@ const app = Vue.createApp({
         startEdit(index) {
             this.tasks[index].editing = true;
             this.tasks[index].editText = this.tasks[index].text;
+            this.tasks[index].editDate = this.tasks[index].date;
+            this.tasks[index].editTime = this.tasks[index].time;
         },
         finishEdit(index) {
             if (this.tasks[index].editText.trim()) {
                 this.tasks[index].text = this.tasks[index].editText;
+                this.tasks[index].date = this.tasks[index].editDate;
+                this.tasks[index].time = this.tasks[index].editTime;
             }
             this.tasks[index].editing = false;
+        },
+        cancelEdit(index) {
+            this.tasks[index].editing = false;
+        },
+        formatDateTime(date, time) {
+            if (!date && !time) return '';
+            let formattedDate = date ? new Date(date).toLocaleDateString() : '';
+            return `${formattedDate} ${time || ''}`.trim();
         }
     }
 });
